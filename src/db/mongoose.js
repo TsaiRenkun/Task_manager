045170c29e5f3e7f8a10b5972014e6b94df35050
Tsairenkun <Tsairenkun@gmail.com>
2020-10-06 15:50:35 +0800

    Creating API "creating user" & "creating task"

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,25 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-});
 
-const me = new User({ name: "Kun", age: 25 });
 
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
