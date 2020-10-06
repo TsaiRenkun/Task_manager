bcf687f85e793d0019624f9dc4e5a7f9b78ec798
Tsairenkun <Tsairenkun@gmail.com>
2020-10-06 12:32:11 +0800

    update, delete on mongoDb

--- mongodb.js
+++ mongodb.js
@@ -14,56 +14,19 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
 
   const db = client.db(databaseName);
 
-//   db.collection("users").findOne(
-//     { _id: new ObjectId("5f7bdd57f3e3d4fd4a2604f7") },
-//     (err, result) => {
-//       if (err) {
-//         return console.log("not found");
-//       } else {
-//         console.log(result);
-//       }
-//     }
-//   );
+//   db.collection('users').deleteMany({
+//       age:24
+//   }).then((result)=>{
+//       console.log(result)
+//   }).then((error)=>{
+//     console.log(error)
+//   })
 
-//   db.collection("users")
-//     .find({ age: 24 })
-//     .toArray((err, result) => {
-//       if (err) {
-//         return console.log("error");
-//       } else {
-//         console.log(result);
-//       }
-//     });
-
-//   db.collection("users")
-//     .find({ age: 24 })
-//     .count((err, result) => {
-//       if (err) {
-//         return console.log("error");
-//       } else {
-//         console.log(result);
-//       }
-//     });
-
-    db.collection("tasks").findOne(
-        { _id: new ObjectId("5f7bdd57f3e3d4fd4a2604f9") },
-        (err, result) => {
-          if (err) {
-            return console.log("not found");
-          } else {
-            console.log(result);
-          }
-        }
-      );
-    
-      db.collection("tasks")
-        .find({ completed: false })
-        .toArray((err, result) => {
-          if (err) {
-            return console.log("error");
-          } else {
-            console.log(result);
-          }
-        });
-    
+  db.collection('tasks').deleteOne({
+      description: "Feed dog"
+  }).then((result)=>{
+      console.log(result)
+  }).then((error)=>{
+      console.log(error)
+  })
 });
