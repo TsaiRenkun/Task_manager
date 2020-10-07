3324fbacb215c1e93aa4ed4605b86b98f2b11717
Tsairenkun <Tsairenkun@gmail.com>
2020-10-07 16:28:50 +0800

    adding Get Tasks and task

--- src/index.js
+++ src/index.js
@@ -1,36 +1,95 @@
-const express = require('express');
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 //Creating new user
-app.post('/users', (req,res)=>{
-    const user = new User(req.body)
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
 
-    user.save().then(()=>{
-        res.status(201).send(user)
-    }).catch((error)=>{
-        res.status(400).send(error)
+  user
+    .save()
+    .then(() => {
+      res.status(201).send(user);
     })
-})
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+//Find users
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
+
+//Find single user
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
 
 //Creating new task
-app.post('/tasks', (req,res)=>{
-    const task = new Task(req.body)
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
 
-    task.save().then(()=>{
-        res.status(201).send(task)
-    }).catch((error)=>{
-        res.status(400).send(error)
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
-})
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
 
-app.listen(port, ()=>{
-    console.log("server is working")
-})
+//Find Tasks
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.status(200).send(tasks);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
+
+//Find task
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
+
+app.listen(port, () => {
+  console.log("server is working");
+});
