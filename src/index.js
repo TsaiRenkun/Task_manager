22d9dee8ce49702a5782b800978241e06be458cb
Tsairenkun <Tsairenkun@gmail.com>
2020-10-14 14:12:31 +0800

    Refactoring and splitting of routes using express Router()

--- src/index.js
+++ src/index.js
@@ -1,94 +1,16 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(userRouter)
+app.use(taskRouter)
 
-//Creating new user
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-
-  // user
-  //   .save()
-  //   .then(() => {
-  //     res.status(201).send(user);
-  //   })
-  //   .catch((err) => {
-  //     res.status(400).send(err);
-  //   });
-});
-
-//Find users
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-//Find single user
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-//Creating new task
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(err);
-  }
-});
-
-//Find Tasks
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).send(tasks);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-//Find task
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
 
 app.listen(port, () => {
   console.log("server is working");
