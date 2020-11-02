fb62e1a51311597a8386bd1d1e49057e07ab4eeb
Tsairenkun <Tsairenkun@gmail.com>
2020-11-02 14:34:54 +0800

    adding avatar and error handling

--- src/index.js
+++ src/index.js
@@ -7,27 +7,27 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const multer = require("multer");
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error("Please upload pdf"));
-    }
+// const multer = require("multer");
+// const upload = multer({
+//   dest: "images",
+//   limits: {
+//     fileSize: 1000000,
+//   },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(doc|docx)$/)) {
+//       return cb(new Error("Please upload pdf"));
+//     }
 
-    cb(undefined, true);
-    // cb(new Error('File must be a PDF'))
-    // cb(undefined, true)
-    // cb(undefined, false)
-  },
-});
+//     cb(undefined, true);
+//     // cb(new Error('File must be a PDF'))
+//     // cb(undefined, true)
+//     // cb(undefined, false)
+//   },
+// });
 
-app.post("/upload", upload.single("upload"), (req, res) => {
-  res.send();
-});
+// app.post("/upload", upload.single("upload"), (req, res) => {
+//   res.send();
+// });
 
 app.use(express.json());
 app.use(userRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,6 @@ const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
 
-const upload = multer({ dest: "avater" });
 //Creating new user
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -76,6 +75,19 @@ router.get("/users", auth, async (req, res) => {
 router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
+
+const upload = multer({
+  dest:'avatar',
+  // limits: {
+  //   fileSize: 1000000
+  // },
+  fileFilter(req, file, cb){
+    if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+      return cb(new Error("Please upload an image"))
+    }
+    cb(undefined, true);
+  }
+})
 
 router.post("/users/me/avatar", upload.single('avatar'), (req,res)=>{
   res.send()
