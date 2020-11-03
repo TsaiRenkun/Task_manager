0949a10214863ea186732ffee07fd16d1e9061c7
Tsairenkun <Tsairenkun@gmail.com>
2020-11-03 14:59:54 +0800

    Creating an example where it handles express errors

--- src/index.js
+++ src/index.js
@@ -7,27 +7,34 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-// const multer = require("multer");
-// const upload = multer({
-//   dest: "images",
-//   limits: {
-//     fileSize: 1000000,
-//   },
-//   fileFilter(req, file, cb) {
-//     if (!file.originalname.match(/\.(doc|docx)$/)) {
-//       return cb(new Error("Please upload pdf"));
-//     }
+const multer = require("multer");
+const upload = multer({
+  dest: "images",
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please upload pdf"));
+    }
 
-//     cb(undefined, true);
-//     // cb(new Error('File must be a PDF'))
-//     // cb(undefined, true)
-//     // cb(undefined, false)
-//   },
-// });
+    cb(undefined, true);
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cb(undefined, false)
+  },
+});
 
-// app.post("/upload", upload.single("upload"), (req, res) => {
-//   res.send();
-// });
+const errorMiddleware = (req,res,next)=>{
+  throw new Error('From my middleware')
+}
+
+app.post("/upload",upload.single('upload'), (req, res) => {
+  res.send();
+  
+}, (error, req, res, next)=>{
+  res.status(400).send({error: error.message})
+});
 
 app.use(express.json());
 app.use(userRouter);
