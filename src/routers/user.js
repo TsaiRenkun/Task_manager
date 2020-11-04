b2758e69603c127c0822eae936bd7107cd5aac2c
Tsairenkun <Tsairenkun@gmail.com>
2020-11-04 13:48:29 +0800

    Creating route to allow user to retrieve avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -81,28 +81,34 @@ const upload = multer({
   // limits: {
   //   fileSize: 1000000
   // },
-  fileFilter(req, file, cb){
-    if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
-      return cb(new Error("Please upload an image"))
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image"));
     }
     cb(undefined, true);
-  }
-})
-
-router.post("/users/me/avatar", auth ,upload.single('avatar'), async(req,res)=>{
-  req.user.avatar = req.file.buffer
-  await req.user.save()
-  res.send()
-},(error, req,res,next)=>{
-  res.status(400).send({error: error.message})
+  },
 });
 
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
 //delete avatar
-router.delete("/users/me/avatar", auth, async(req,res)=>{
-  req.user.avatar = undefined
-  await req.user.save()
-  res.send("hello delete avatar route working")
-})
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send("hello delete avatar route working");
+});
 
 //update user
 router.patch("/users/me", auth, async (req, res) => {
@@ -137,6 +143,19 @@ router.delete("/users/me", auth, async (req, res) => {
   }
 });
 
+router.get("/users/:id/avatar", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
 
+    if(!user || !user.avatar){
+      throw new Error()
+    }
+
+    res.set('Content-Type','image/jpg')
+    res.send(user.avatar)
+  } catch (e) {
+    res.status(400).send()
+  }
+});
 
 module.exports = router;
