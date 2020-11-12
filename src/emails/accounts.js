ad81782bb25626f4acd701b8a25430d00d161c82
Tsairenkun <Tsairenkun@gmail.com>
2020-11-12 12:36:02 +0800

    sending auto emails when delete

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -4,9 +4,33 @@ const sendgridAPIKey = 'SG.w8GrHUisRwiG_J58LZ5QiQ.s6YFEyNsgjCkM5tlyZf1xjfdyxmBsJ
 
 sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: "dracrogo@gmail.com",
-    from: 'tsairenkun@gmail.com',
-    subject: 'I hope it gets to me',
-    text: "Reach me please"
-})
+
+const sendWelcomeEmail = (email,name)=>{
+    sgMail.send({
+        to:email,
+        from: 'tsairenkun@gmail.com',
+        subject: 'Thanks for joining',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app`
+    })
+}
+
+const cancellationFeedback = (email, name)=>{
+    sgMail.send({
+        to:email,
+        from: 'tsairenkun@gmail.com',
+        subject: "Your feedback is apperciated",
+        text: `Do let us know how we could have kept you on the app ${name}, Thank you`
+    })
+}
+
+// sgMail.send({
+//     to: "dracrogo@gmail.com",
+//     from: 'tsairenkun@gmail.com',
+//     subject: 'I hope it gets to me',
+//     text: "Reach me please"
+// })
+
+module.exports = {
+    sendWelcomeEmail,
+    cancellationFeedback
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const sharp = require("sharp")
 const User = require("../models/user");
 const auth = require("../middleware/auth");
+const { sendWelcomeEmail, cancellationFeedback } = require("../emails/accounts");
 const router = new express.Router();
 
 //Creating new user
@@ -11,6 +12,7 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name)
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
@@ -138,6 +140,7 @@ router.patch("/users/me", auth, async (req, res) => {
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
+    cancellationFeedback(req.user.email, req.user.name)
     res.send(req.user);
   } catch (e) {
     res.status(400).send();
